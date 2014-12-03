45dfe62c38325c3eee1ff2a67c51dfe2d9c5ac28
shenghai <456@789.com>
2014-12-03 13:51:01 +0800

    cvcvcv

---
+++ test/test.cpp
@@ -0,0 +1,14 @@
+// test.cpp : Defines the entry point for the console application.
+//
+#include <stdlib.h>
+#include "stdafx.h"
+#include <iostream>
+using namespace std;
+
+int _tmain(int argc, _TCHAR* argv[])
+{
+	cout<< "Hello World" << endl;
+	system("pause");
+	return 0;
+}
+
